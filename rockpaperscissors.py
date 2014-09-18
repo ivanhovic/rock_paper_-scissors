ba064004b33e70d6a6cbb638971e2e127300d96a
Ivan Murillo <murilloivan@gmail.com>
2014-09-18 02:06:14 +0200

    python3 correction

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,13 +1,3 @@
-# The key idea of this program is to equate the strings
-# "rock", "paper", "scissors", "lizard", "Spock" to numbers
-# as follows:
-#
-# 0 - rock
-# 1 - Spock
-# 2 - paper
-# 3 - lizard
-# 4 - scissors
-
 import random
 
 def number_to_name(number):
@@ -28,7 +18,7 @@ def number_to_name(number):
     elif number == 4:
         name = "scissors"
         return name
-    else: print "you need to imput a number between 0-4"
+    else: print ("you need to imput a number between 0-4")
 
 def name_to_number(name):
     # convert name to number using if/elif/else
@@ -48,34 +38,34 @@ def name_to_number(name):
     elif name == "scissors":
         number = 4
         return number
-    else: print "you need to imput a correct string"
+    else: print ("you need to imput a correct string")
 
 def rpsls(name):
 
     # convert name to player_number using name_to_number
-    print "Player chooses", name
+    print ("Player chooses", name)
     number = name_to_number(name)
     
     # compute random guess for comp_number using random.randrange()
     comp_number = random.randrange(0,5,1)
-    print "Computer chooses", number_to_name(comp_number)
+    print ("Computer chooses", number_to_name(comp_number))
     
     # compute difference of player_number and comp_number modulo five
     # use if/elif/else to determine winner
     if number == comp_number:
-        print "Player and computer tie!"
+        print ("Player and computer tie!")
     elif (number - comp_number)%5<=2:
-        print "PLAYER WINS!!"
-    else: print "COMPUTER WINS!!"
+        print ("PLAYER WINS!!")
+    else: print ("COMPUTER WINS!!")
 
     # convert comp_number to name using number_to_name
     # print results
-    print ""
+    print ("")
 
 
 def play():
-    print 'rock, spock, paper, lizard or scissors?'
-    name = raw_input()
+    print ('rock, spock, paper, lizard or scissors?')
+    name = input()
     rpsls(name)
 
 play()
